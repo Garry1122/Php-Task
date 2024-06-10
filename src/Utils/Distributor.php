39cbf324898ac49a621deb5d6bded5acbeb36024
Garry112 <garik,nahapetyan112@gmail.com>
2024-06-11 02:08:10 +0400

    Updated application logic

--- src/Utils/Distributor.php
+++ src/Utils/Distributor.php
@@ -6,21 +6,17 @@ class Distributor
     public static function distributeDeals(array &$operators, array &$deals): void
     {
         $totalDeals = $deals['DEAL'];
-        $totalOperators = count($operators);
-        $dealsPerOperator = intdiv($totalDeals, $totalOperators);
 
-        foreach ($operators as $operator => $count) {
-            $operators[$operator] += $dealsPerOperator;
-            $deals['DEAL'] -= $dealsPerOperator;
-        }
+        asort($operators);
 
-        foreach ($operators as $operator => $count) {
-            if ($deals['DEAL'] > 0) {
-                $operators[$operator]++;
-                $deals['DEAL']--;
+        while ($totalDeals > 0) {
+            foreach ($operators as $operator => $count) {
+                if ($totalDeals > 0) {
+                    $operators[$operator]++;
+                    $deals['DEAL']--;
+                    $totalDeals--;
+                }
             }
         }
     }
 }
-
-
